c775457e9e1ec84131413a7a2ad1f27adcf93479
NIKFIRE4 <nikitakostrov96@gmail.com>
2025-06-11 01:40:29 +0300

    Make function get_important_information

--- ML_agent/my_openrouter_chat.py
+++ ML_agent/my_openrouter_chat.py
@@ -1,14 +1,14 @@
 from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Any
 import os
 
 class FishingRequest(BaseModel):
-    wish_fishes: Optional[List[str]] = None
-    fishing_place: Optional[str] = None
-    location_user: Optional[str] = None
-    wish_radius_km: Optional[int] = None
+    fishing_location: Optional[List[str]] = []
+    caught_fishes: Optional[List[str]] = []
+    coordinates: Optional[List[float]] = []
+    water_space: Optional[List[str]] = []
 
 llm = ChatOpenAI(
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
@@ -17,16 +17,16 @@ llm = ChatOpenAI(
     temperature=0.7,
 )
 
-structured_llm = llm.with_structured_output(FishingRequest)
+def get_important_information(message: str):
+    structured_llm = llm.with_structured_output(FishingRequest)
+    messages = [
+    SystemMessage(content="Извлеки информацию о рыбалке из сообщения пользователя, в переменную fishing_location нужно записать место где человек рыбачил, например ['Река Вуокса Выборгский район'], в переменную caught_fishes ты должен записать рыб, которых пользователь поймал на этой локации, например ['щука', 'окунь'], в переменную coordinates нужно записать координаты точки где рыбак ловил рыбу, обычно это выглядит как-то так: [59.861234, 30.154855] В переменную water_space нужно записать вид водоема в котором человек рыбачил, например ['озеро'] или ['река']."),
+    HumanMessage(content=message)]
+    result = structured_llm.invoke(messages)
+    return {
+        "fishing_location": result.fishing_location,
+        "coordinates": result.coordinates,
+        "caught_fishes": result.caught_fishes,
+        "water_space": result.water_space,
+    }
 
-messages = [
-    SystemMessage(content="Извлеки информацию о рыбалке из сообщения пользователя"),
-    HumanMessage(content="Я хочу порыбачить в 20 км от станции метро Автово в Санкт-Петербурге, желательно поймать щуку или окуня, предпочтительно на речке,где это можно сделать?"),
-]
-
-result = structured_llm.invoke(messages)
-
-print(f"Желаемая рыба: {result.wish_fishes}")
-print(f"Место рыбалки: {result.fishing_place}")
-print(f"Местоположение: {result.location_user}")
-print(f"Радиус: {result.wish_radius_km} км")
