836382b03131fc591179b53bec08ae23462ce034
NIKFIRE4 <138225574+NIKFIRE4@users.noreply.github.com>
2025-06-10 12:16:21 +0300

    Delete ML_agent/CV_for_person_detect directory from main

--- ML_agent/CV_for_person_detect/YOLO_predict.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(r"cv_for_person_detect/yolov5n.pt")
-
-def detect_person(img_path: str, conf: float = 0.58) -> bool:
-    res = model(img_path, classes=[0], conf=conf, save=True)
-    print(res[0].boxes.cls.tolist())
-    return bool(res[0].boxes.cls.tolist())
-
