94d24c27eb25402a4ce2b40275fa380be23dbf14
NIKFIRE4 <nikitakostrov96@gmail.com>
2025-06-11 02:40:39 +0300

    add endpoint for get info from table places_with_fish

--- ML_agent/main.py
+++ ML_agent/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from endpoints.classes_for_endpoint import ImageRequest
 import uvicorn
+import httpx
 from CV_for_person_detect.YOLO_predict import detect_person
 
 app = FastAPI(title="Person Detection API", version="1.0.0")
@@ -22,6 +23,25 @@ async def detect_person_endpoint(request: ImageRequest):
         
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Detection failed: {str(e)}")
+    
+
+@app.get("/places_with_fish")
+async def get_fishing_places():
+    BACKEND_URL = "http://192.168.0.103:8002"
+    
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(BACKEND_URL, timeout=10.0)
+            response.raise_for_status()  # если статус >=400 — бросит exception
+            data = response.json()
+            print(data)
+            return JSONResponse(content=data)
+    
+    except (httpx.HTTPError, ValueError) as e:
+        raise HTTPException(
+            status_code=502,
+            detail=f"Ошибка при получении рыбацких мест: {str(e)}"
+        )
 
 if __name__ == "__main__":
     uvicorn.run(
