d41f9c2b8b396883af32cd9c02545661f0a1b458
NIKFIRE4 <nikitakostrov96@gmail.com>
2025-07-22 13:15:10 +0300

    add endpoint telegram/search

--- ML_agent/endpoints/classes_for_endpoint.py
+++ ML_agent/endpoints/classes_for_endpoint.py
@@ -1,6 +1,29 @@
 from pydantic import BaseModel
+from typing import *
 class ImageRequest(BaseModel):
     image: str 
 
 class MessageRequest(BaseModel):
     message: str
+
+class TelegramSearchRequest(BaseModel):
+    user_id: int
+    query: str
+
+class FishingSpot(BaseModel):
+    name: str
+    location_user: List[float]
+    description: str
+    short_description: dict
+    coordinates: List[float]
+    distance_km: Optional[float] = None
+
+class TelegramSearchResponse(BaseModel):
+    success: bool
+    spots: List[FishingSpot]
+    message: Optional[str] = None
+    user_location: Optional[str] = None
+
+class BestPlacesRequest(BaseModel):
+    target_fish: List[str]    
+    water_space: List[str]
